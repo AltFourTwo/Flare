e5defdcb19db8713fb2311a8e11e260adad3efc9
Alex <altfourtwo@github.com>
2020-03-06 22:52:19 -0500

    Progress into message formatting for the console
    Plus const reference in many places to avoid unecessary copying
    Nested Struct "FormatAction", might make this one its own class.

--- Utility/src/Logging/Logger.cpp
+++ Utility/src/Logging/Logger.cpp
@@ -5,6 +5,22 @@
 
 namespace Logging
 {
+   /**********************\
+   \*****   NESTED   *****/
+   Logger::FormatAction::FormatAction( const FormatAction::ReturnType& a_ReturnType, const char& a_FormatChar ) :
+      m_FormatChar(a_FormatChar),
+      m_ReturnType(a_ReturnType),
+      m_ReturnText()
+   {
+      m_ReturnText.reserve( 25 );
+   }
+
+   Logger::FormatAction::FormatAction( const std::string& a_Text ) :
+      m_FormatChar( 0 ),
+      m_ReturnType( JUST_TEXT ),
+      m_ReturnText( a_Text )
+   {}
+
    /**************************************\
    \*****   CONSTRUCTOR-DESTRUCTOR   *****/
    Logger::Logger( const char*& a_LoggerName, const LogLevel a_BaseLoggingLevel, const int& a_TextColor, const int& a_BGColor, const std::string& a_LoggingFormat ) :
@@ -12,14 +28,15 @@ namespace Logging
       m_BaseLoggingLevel( a_BaseLoggingLevel ),
       m_TextColor( a_TextColor ),
       m_BGColor( a_BGColor ),
-      m_LoggingFormat( a_LoggingFormat )
+      m_ExecutionQueue()
    {
+      CompileFormat( m_ExecutionQueue, a_LoggingFormat );
       // This->Log("Logger Created!");
    }
 
    /********************************\
    \*****   PUBLIC-FUNCTIONS   *****/
-   // Without Formattables
+   // Log Without Formattables
    void Logger::Trace( const char*& a_Message )
    {
       if ( m_BaseLoggingLevel <= TRACE )
@@ -56,52 +73,86 @@ namespace Logging
          Logger::Log( FATAL, a_Message );
    }
 
-   // With Formattables
-   void Logger::Trace( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   // Log With Formattables
+   void Logger::Trace( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       if ( m_BaseLoggingLevel <= TRACE )
          Logger::Log( TRACE, a_Message, a_Formattables );
    }
 
-   void Logger::Debug( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   void Logger::Debug( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       if ( m_BaseLoggingLevel <= DEBUG )
          Logger::Log( DEBUG, a_Message, a_Formattables );
    }
 
-   void Logger::Info( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   void Logger::Info( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       if ( m_BaseLoggingLevel <= INFO )
          Logger::Log( INFO, a_Message, a_Formattables );
    }
 
-   void Logger::Warn( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   void Logger::Warn( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       if ( m_BaseLoggingLevel <= WARNING )
          Logger::Log( WARNING, a_Message, a_Formattables );
    }
 
-   void Logger::Error( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   void Logger::Error( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       if ( m_BaseLoggingLevel <= ERROR )
          Logger::Log( ERROR, a_Message, a_Formattables );
    }
 
-   void Logger::Fatal( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   void Logger::Fatal( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       if ( m_BaseLoggingLevel <= FATAL )
          Logger::Log( FATAL, a_Message, a_Formattables );
    }
 
+   /***********************\
+   \*****   SETTERS   *****/
+   void Logger::SetBaseLoggingLevel( const LogLevel& a_LoggingLevel )
+   {
+      m_BaseLoggingLevel = a_LoggingLevel;
+   }
+
+   void Logger::SetTextColor( const int& a_TextColor )
+   {
+      m_TextColor = a_TextColor;
+   }
+
+   void Logger::SetBackgroundColor( const int& a_BGColor )
+   {
+      m_BGColor = a_BGColor;
+   }
+
+   void Logger::SetFormat( const std::string& a_Format )
+   {
+      CompileFormat( m_ExecutionQueue, a_Format );
+   }
+
    /*********************************\
    \*****   PRIVATE-FUNCTIONS   *****/
-   void Logger::Log( LogLevel a_LogLevel, const char*& a_Message )
+   void Logger::Log( const LogLevel& a_LogLevel, const char*& a_Message )
    {
       Console::Get().Log( *this, a_LogLevel, a_Message );
    }
 
-   void Logger::Log( LogLevel a_LogLevel, const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables )
+   void Logger::Log( const LogLevel& a_LogLevel, const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables )
    {
       Console::Get().Log( *this, a_LogLevel, a_Message, a_Formattables );
+   }
+
+   void Logger::CompileFormat( std::queue<FormatAction, class queue>& a_ExecutionQueue, const std::string& a_FormatString )
+   {
+      a_ExecutionQueue.empty();
+
+      // TODO Finish Implementing This
+
+      /* Examples */
+      a_ExecutionQueue.emplace( FormatAction::ReturnType::FORMATTED_DATE, '!' );
+      a_ExecutionQueue.emplace( FormatAction::ReturnType::LOGGER_INFO, '!' );
+      a_ExecutionQueue.emplace( "Oh no!" );
    }
 }
--- Utility/src/Logging/Logger.h
+++ Utility/src/Logging/Logger.h
@@ -9,14 +9,33 @@ namespace Logging
    \*****   CLASS LOGGER   *****/
    class Logger
    {
+      /**********************\
+      \*****   NESTED   *****/
+      private:
+      struct FormatAction
+      {
+         enum ReturnType
+         {
+            FORMATTED_DATE = 1,
+            LOGGER_INFO = 2,
+            JUST_TEXT = 3
+         };
+         char m_FormatChar;
+         ReturnType m_ReturnType;
+         std::string m_ReturnText;
+
+         FormatAction( const ReturnType& a_ReturnType, const char& a_FormatChar );
+         FormatAction( const std::string& a_Text );
+      };
+
       /*******************************\
       \*****   PRIVATE-MEMBERS   *****/
       private:
       const char* m_LoggerName;
-      const LogLevel m_BaseLoggingLevel;
-      const int m_TextColor;
-      const int m_BGColor;
-      const std::string m_LoggingFormat;
+      LogLevel m_BaseLoggingLevel;
+      int m_TextColor; // TODO Find a better type for color properties, int is a placeholder type.
+      int m_BGColor;
+      std::queue<FormatAction, class queue> m_ExecutionQueue;
 
       /**************************************\
       \*****   CONSTRUCTOR-DESTRUCTOR   *****/
@@ -26,23 +45,33 @@ namespace Logging
       /********************************\
       \*****   PUBLIC-FUNCTIONS   *****/
       public:
+      // Log Functions
       void Trace( const char*& a_Message );
       void Debug( const char*& a_Message );
       void Info( const char*& a_Message );
       void Warn( const char*& a_Message );
       void Error( const char*& a_Message );
       void Fatal( const char*& a_Message );
-      void Trace( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
-      void Debug( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
-      void Info( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
-      void Warn( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
-      void Error( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
-      void Fatal( const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
+      void Trace( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+      void Debug( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+      void Info( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+      void Warn( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+      void Error( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+      void Fatal( const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+
+      /***********************\
+      \*****   SETTERS   *****/
+      public:
+      void SetBaseLoggingLevel( const LogLevel& a_LoggingLevel );
+      void SetTextColor( const int& a_TextColor );
+      void SetBackgroundColor( const int& a_BGColor );
+      void SetFormat( const std::string& a_Format );
 
       /*********************************\
       \*****   PRIVATE-FUNCTIONS   *****/
       private:
-      void Log( LogLevel a_LogLevel, const char*& a_Message );
-      void Log( LogLevel a_LogLevel, const char*& a_Message, std::initializer_list<Composing::Formattable> a_Formattables );
+      void Log( const LogLevel& a_LogLevel, const char*& a_Message );
+      void Log( const LogLevel& a_LogLevel, const char*& a_Message, const std::initializer_list<Composing::Formattable>& a_Formattables );
+      void CompileFormat( std::queue<FormatAction, class queue>& a_ExecutionQueue, const std::string& a_LoggingFormat );
    };
 }
