dd615c027edb3d415052f63b632ab1b55b8c465f
AltFourTwo <aleduc.code@gmail.com>
2020-04-24 20:55:56 -0400

    BitFlags complete for now

--- Utility/src/BitFlags/BitFlags.cpp
+++ Utility/src/BitFlags/BitFlags.cpp
@@ -2,54 +2,151 @@
 
 #include <cstdint>
 #include <type_traits>
+#include <bitset>
 
+#include "BitMasks.h"
 #include "BitFlags.h"
 
 namespace BitFlags
 {
+   /***** BitReference *****/
    /*****   CLASS   C-TOR D-TOR  *****/
    template<typename T>
-   ByteFlags<T>::ByteFlags( T a_IntegralValue, typename as_integral_t<T>* )
-      : m_Flags( a_IntegralValue )
-   {
-
-   }
+   BitReference<T>::BitReference( const T& a_Flags, const T&& a_Mask ) :
+      m_Flags( a_Flags ),
+      m_Mask( a_Mask )
+      EMPTY_SCOPE;
 
    /*****   CLASS   FUNCTIONS    *****/
    template<typename T>
-   void ByteFlags<T>::SetAllFalse()
+   void BitReference<T>::Set()
+   {
+      m_Flags |= m_Mask;
+   }
+
+   template<typename T>
+   void BitReference<T>::Set( const bool a_Value )
+   {
+      if ( a_Value )
+         Set();
+      else
+         Reset();
+   }
+
+   template<typename T>
+   void BitReference<T>::Reset()
+   {
+      m_Flags &= ~m_Mask;
+   }
+
+   template<typename T>
+   void BitReference<T>::Flip()
+   {
+      Set( !Value() );
+   }
+
+   template<typename T>
+   bool BitReference<T>::Value() const
+   {
+      return m_Flags & m_Mask;
+   }
+
+   /***** ByteFlags *****/
+   /*****   CLASS   C-TOR D-TOR  *****/
+   template<typename T>
+   ByteFlags<T>::ByteFlags() :
+      m_Flags(0)
+   {}
+
+   template<typename T>
+   ByteFlags<T>::ByteFlags( T a_IntegralValue ) :
+      m_Flags( a_IntegralValue )
+   {}
+
+
+   /*****   CLASS   FUNCTIONS    *****/
+   template<typename T>
+   const T&& ByteFlags<T>::GetBitMask( const uint8_t& a_Pos )
+   {
+      T x_Bit = 1;
+
+      return ( x_Bit << a_Pos );
+   }
+
+   template<typename T>
+   void ByteFlags<T>::Set()
+   {
+      m_Flags = UINT64_MAX;
+   }
+
+   template<typename T>
+   void ByteFlags<T>::Set( const uint8_t& a_Pos )
+   {
+      m_Flags |= GetBitMask( a_Pos );
+   }
+
+   template<typename T>
+   void ByteFlags<T>::Set( const uint8_t& a_Pos, const bool& a_Value )
+   {
+      if ( a_Value )
+         Set( a_Pos );
+      else
+         Reset( a_Pos );
+   }
+
+   template<typename T>
+   void ByteFlags<T>::Reset()
    {
       m_Flags = 0;
    }
 
    template<typename T>
-   void ByteFlags<T>::SetAllTrue()
+   void ByteFlags<T>::Reset( const uint8_t& a_Pos )
    {
-      m_Flags = this->MaxIntegralValue();
+      m_Flags &= ~GetBitMask( a_Pos );
    }
 
    template<typename T>
-   void ByteFlags<T>::SetFlags( T a_Flags, typename as_integral_t<T>* )
+   void ByteFlags<T>::Flip()
    {
-      m_Flags = a_Flags;
+      m_Flags ~= m_Flags;
    }
 
    template<typename T>
-   size_t ByteFlags<T>::FlagsSize()
+   void ByteFlags<T>::Flip( const uint8_t& a_Pos )
    {
-      return sizeof( T );
+      Set( a_Pos, !Value( a_Pos ) );
    }
 
    template<typename T>
-   T ByteFlags<T>::MaxIntegralValue()
+   bool ByteFlags<T>::AreAllOn()
    {
-      switch ( this->FlagsSize() )
-      {
-         case 1: return UINT8_MAX;
-         case 2: return UINT16_MAX;
-         case 4: return UINT32_MAX;
-         case 8: return UINT64_MAX;
-         default: throw; // TODO Exception Unexpected Number of bytes
-      }
+      return m_Flags == UINT64_MAX;
+   }
+
+   template<typename T>
+   bool ByteFlags<T>::AreAllOff()
+   {
+      return m_Flags == 0;
+   }
+
+   template<typename T>
+   T ByteFlags<T>::Value()
+   {
+      return m_Flags;
+   }
+
+   template<typename T>
+   bool ByteFlags<T>::Value( const uint8_t& a_Pos )
+   {
+      T x_Mask = GetBitMask( a_Pos );
+
+      return m_Flags & x_Mask;
+   }
+
+   template<typename T>
+   BitReference<T> ByteFlags<T>::BitRef( const int& a_Pos )
+   {
+      return BitReference( m_Flags, GetBitMask( a_Pos ) );
    }
 }
--- Utility/src/Macros.h
+++ Utility/src/Macros.h
@@ -2,5 +2,6 @@
 
 // This macro is used whenever a constructor has no logic and only initialize its members.
 // The braces are required even when empty, but Visual Studio places them on a new line and
-// there is no option to have them on the same line when empty.
+// there is no option to have them on the same line when empty. 
+// Should appear with a ; right after to prevent further indentation issues.
 #define EMPTY_SCOPE {}
